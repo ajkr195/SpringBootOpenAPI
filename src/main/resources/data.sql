c38c6d69c54e71be46bdbe42dc67b45a8bc0278f
Ajay Kumar <Ajay Kumar@10.0.0.50>
2022-04-26 08:04:59 -0400

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO employee (firstName,lastname,phoneNumber, email, salary, department) VALUES ('fname1', 'lname1', '123456', 'some@email.com',12345.67, 'HUMAN_RESOURCE');
-INSERT INTO employee (firstName,lastname,phoneNumber, email, salary, department) VALUES ('fname2', 'lname2', '123456', 'some@email.com',12345.67, 'MARKETING');
-INSERT INTO employee (firstName,lastname,phoneNumber, email, salary, department) VALUES ('fname3', 'lname3', '123456', 'some@email.com',12345.67, 'HUMAN_RESOURCE');
-INSERT INTO employee (firstName,lastname,phoneNumber, email, salary, department) VALUES ('fname4', 'lname4', '123456', 'some@email.com',12345.67, 'ENGINEERING');
-INSERT INTO employee (firstName,lastname,phoneNumber, email, salary, department) VALUES ('fname5', 'lname5', '123456', 'some@email.com',12345.67, 'MARKETING');
+INSERT INTO employee (firstname,lastname,phonenumber, email, salary, department) VALUES ('fname1', 'lname1', '123456', 'some@email.com',12345.67, 'HUMAN_RESOURCE');
+INSERT INTO employee (firstname,lastname,phonenumber, email, salary, department) VALUES ('fname2', 'lname2', '123456', 'some@email.com',12345.67, 'MARKETING');
+INSERT INTO employee (firstname,lastname,phonenumber, email, salary, department) VALUES ('fname3', 'lname3', '123456', 'some@email.com',12345.67, 'HUMAN_RESOURCE');
+INSERT INTO employee (firstname,lastname,phonenumber, email, salary, department) VALUES ('fname4', 'lname4', '123456', 'some@email.com',12345.67, 'ENGINEERING');
+INSERT INTO employee (firstname,lastname,phonenumber, email, salary, department) VALUES ('fname5', 'lname5', '123456', 'some@email.com',12345.67, 'MARKETING');
